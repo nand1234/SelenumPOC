74d150a19a2f40ef3e4e24208f0f0e42e1329525
John Doe <john@sample.com>
2017-09-17 15:58:09 +0530

    Added Logic

--- VievU/SeleniumTest.cs
+++ VievU/SeleniumTest.cs
@@ -3,7 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using VievU.UIPageRepository;
 using VievU.WebDriverFactory;
-
+using VievU.ActionKeywords;
+using VievU.TestData;
 
 namespace VievU
 {
@@ -17,37 +18,70 @@ namespace VievU
             Firefox
         }
 
+        private String URL = "https://demo.vievusolution.com/";
+
+        [TestInitialize]
+        public void importTestdata()
+        {
+            ImportTestData import = new ImportTestData();
+            import.inportTestData();
+        }
+        
+
+
         [TestMethod]
         public  void IETest()
         {
           IWebDriver driver=  WebDriverFactory.WebDriver.GetBrowser(Browser.IE.ToString());
-          WebDriverFactory.WebDriver.launchURL("https://www.facebook.com/");
+          WebDriverFactory.WebDriver.launchURL(URL);
 
             LoginUI Ui = new LoginUI();
-            String Username = Ui.GetPasswordLocatorType();
-            String Password = Ui.GetPasswordLocatorType();
-            IWebElement username = GetWebDriverLocator.FindElement("Id","email",driver);
-            username.SendKeys("Nandkumar.kumar@facebook.com");
+            String Usernametype = Ui.GetPasswordLocatorType();
+            String Passwordtype = Ui.GetPasswordLocatorType();
+            String UsernameValue = Ui.GetUserNameLocatorvalue();
+            String paswordValue = Ui.GetPasswordLocatorvalue();
+            String SubmitlocatorType = Ui.Submitlocatortype();
+            String submitLocatorValue = Ui.SubmitlocatorValue();
+            IWebElement username = GetWebDriverLocator.FindElement(Usernametype, UsernameValue, driver);
+            IWebElement Submit = GetWebDriverLocator.FindElement(SubmitlocatorType, submitLocatorValue, driver);
+            KeyboardActions.SendKeys(username, "Admin", driver);
+            MouseActions.Click(Submit, driver);
+            IWebElement Password = GetWebDriverLocator.FindElement(Passwordtype, paswordValue, driver);
+            KeyboardActions.SendKeys(Password, "Vievu", driver);
+            IWebElement login = GetWebDriverLocator.FindElement(SubmitlocatorType, submitLocatorValue, driver);
+            MouseActions.Click(login, driver);
+
+
+
 
         }
         [TestMethod]
         public void FFTest()
         {
             IWebDriver driver = WebDriverFactory.WebDriver.GetBrowser(Browser.Firefox.ToString());
-            WebDriverFactory.WebDriver.launchURL("https://www.facebook.com/");
-
+            WebDriverFactory.WebDriver.launchURL(URL);
             LoginUI Ui = new LoginUI();
-            String Username = Ui.GetPasswordLocatorType();
-            String Password = Ui.GetPasswordLocatorType();
-            IWebElement username = GetWebDriverLocator.FindElement("Id", "email", driver);
-            username.SendKeys("Nandkumar.kumar@facebook.com");
+            String Usernametype = Ui.GetPasswordLocatorType();
+            String Passwordtype = Ui.GetPasswordLocatorType();
+            String UsernameValue = Ui.GetUserNameLocatorvalue();
+            String paswordValue = Ui.GetPasswordLocatorvalue();
+            String SubmitlocatorType = Ui.Submitlocatortype();
+            String submitLocatorValue = Ui.SubmitlocatorValue();
+            IWebElement username = GetWebDriverLocator.FindElement(Usernametype, UsernameValue, driver);
+            IWebElement Submit = GetWebDriverLocator.FindElement(SubmitlocatorType, submitLocatorValue, driver);
+            KeyboardActions.SendKeys(username, "Admin", driver);
+            MouseActions.Click(Submit, driver);
+            IWebElement Password = GetWebDriverLocator.FindElement(Passwordtype, paswordValue, driver);
+            KeyboardActions.SendKeys(Password, "Vievu", driver);
+            IWebElement login = GetWebDriverLocator.FindElement(SubmitlocatorType, submitLocatorValue, driver);
+            MouseActions.Click(login, driver);
 
         }
         [TestMethod]
         public void ChromeTest()
         {
             IWebDriver driver = WebDriverFactory.WebDriver.GetBrowser(Browser.Chrome.ToString());
-            WebDriverFactory.WebDriver.launchURL("https://www.facebook.com/");
+            WebDriverFactory.WebDriver.launchURL(URL);
 
             LoginUI Ui = new LoginUI();
             String Username = Ui.GetPasswordLocatorType();
---
+++ VievU/TestData/ImportTestData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VievU.TestData
+{
+    class ImportTestData
+    {
+        public void inportTestData()
+        {
+            var fileName = string.Format(@"F:\TestData\TestData.xlsx");
+            var connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;data source="+fileName+";Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1;\"");
+
+            var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            adapter.Fill(ds,"Data");
+            dt = ds.Tables["Data"];
+
+            var count = dt.Columns.Count;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                string Module = dr[0].ToString();
+                string Submodule = dr[1].ToString();
+                string TestCase = dr[2].ToString();
+                string ExpectedResult = dr[3].ToString();
+                string FieldName = dr[4].ToString();
+                string FieldValue = dr[5].ToString();
+               
+            }
+
+
+        }
+    }
+}
