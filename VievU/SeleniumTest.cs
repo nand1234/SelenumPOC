2505ad5d5e567f9f01a8524925069f19c4103b38
John Doe <john@sample.com>
2017-09-17 16:38:47 +0530

    added: ChromeTest

--- VievU/SeleniumTest.cs
+++ VievU/SeleniumTest.cs
@@ -80,14 +80,23 @@ namespace VievU
         [TestMethod]
         public void ChromeTest()
         {
-            IWebDriver driver = WebDriverFactory.WebDriver.GetBrowser(Browser.Chrome.ToString());
+            IWebDriver driver = WebDriverFactory.WebDriver.GetBrowser(Browser.Firefox.ToString());
             WebDriverFactory.WebDriver.launchURL(URL);
-
             LoginUI Ui = new LoginUI();
-            String Username = Ui.GetPasswordLocatorType();
-            String Password = Ui.GetPasswordLocatorType();
-            IWebElement username = GetWebDriverLocator.FindElement("Id", "email", driver);
-            username.SendKeys("Nandkumar.kumar@facebook.com");
+            String Usernametype = Ui.GetPasswordLocatorType();
+            String Passwordtype = Ui.GetPasswordLocatorType();
+            String UsernameValue = Ui.GetUserNameLocatorvalue();
+            String paswordValue = Ui.GetPasswordLocatorvalue();
+            String SubmitlocatorType = Ui.Submitlocatortype();
+            String submitLocatorValue = Ui.SubmitlocatorValue();
+            IWebElement username = GetWebDriverLocator.FindElement(Usernametype, UsernameValue, driver);
+            IWebElement Submit = GetWebDriverLocator.FindElement(SubmitlocatorType, submitLocatorValue, driver);
+            KeyboardActions.SendKeys(username, "Admin", driver);
+            MouseActions.Click(Submit, driver);
+            IWebElement Password = GetWebDriverLocator.FindElement(Passwordtype, paswordValue, driver);
+            KeyboardActions.SendKeys(Password, "Vievu", driver);
+            IWebElement login = GetWebDriverLocator.FindElement(SubmitlocatorType, submitLocatorValue, driver);
+            MouseActions.Click(login, driver);
 
         }
     }
